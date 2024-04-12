cd165be397b93e8ed31fab862b14de18c7a456cb
MBVLoveless <149451965+MBVLoveless@users.noreply.github.com>
2024-04-12 07:38:55 +0100

    Add files via upload

---
+++ ft_strmapi.c
@@ -0,0 +1,16 @@
+#include <libft.h>
+
+char	*ft_strmapi(char const *s, char(*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char		*new;
+
+	i = -1;
+	new = malloc(sizeof(char) * (ft_strlen(s)) + 1);
+	if (!new)
+		return (0);
+	while(s[++i])
+		new[i] = f(i, s[i]);
+	new[i] = '\0';
+	return (new);
+}
